36a82e73f43a3afe9525b4e061ec3017f6bbfe84
Sergi Blanch-Torné <sergi@calcurco.cat>
2015-08-28 09:23:25 +0200

    add header with license like the other source files.

--- scpi/__init__.py
+++ scpi/__init__.py
@@ -1,1 +1,33 @@
+###############################################################################
+## file :               __init__.py
+##
+## description :        Python module to provide scpi functionality to an 
+##                      instrument.
+##
+## project :            scpi
+##
+## author(s) :          S.Blanch-Torn\'e
+##
+## Copyright (C) :      2015
+##                      CELLS / ALBA Synchrotron,
+##                      08290 Bellaterra,
+##                      Spain
+##
+## This file is part of Tango.
+##
+## Tango is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Tango is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Tango.  If not, see <http:##www.gnu.org/licenses/>.
+##
+###############################################################################
+
 from scpi import scpi
