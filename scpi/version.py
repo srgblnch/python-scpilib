46eaa313615d2dee9bc93d34819e4d501a297bd3
Sergi Blanch-Torné <sergi@calcurco.cat>
2016-05-06 11:24:21 +0200

    fix license headers because this is GPLv3+ but they were saying 2+

--- scpi/version.py
+++ scpi/version.py
@@ -2,7 +2,7 @@
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
+#  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -26,7 +26,7 @@ __license__ = "GPLv3+"
 __MAJOR_VERSION = 0
 __MINOR_VERSION = 2
 __BUILD_VERSION = 0
-__REVISION_VERSION = 0
+__REVISION_VERSION = 1
 __RELEASE_CANDIDATE = None
 
 
